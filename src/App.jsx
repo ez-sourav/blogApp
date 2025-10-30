8375aa6fc995dfc772cd0bfddd7f2e96cbbaf63f
ez-sourav <souravb2003@gmail.com>
2025-10-30 19:17:20 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <div className='bg-red-400 '>
+      react-hook-form
+    </div>
+  )
+}
+
+export default App
