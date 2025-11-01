65de3ea4beff4c37fdd185ac9a16d8490d52d35f
ez-sourav <souravb2003@gmail.com>
2025-11-01 22:29:37 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import { useDispatch } from 'react-redux'
 import React, { useState,useEffect } from 'react'
 import authService from './appwrite/auth' 
+import {login,logout} from './store/authSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   const [loading, setLoading] = useState(true)
   const dispatch = useDispatch() 
-  return (
-    <div className='bg-red-400 '>
-      react-hook-form
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=> {
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+         TODO : <Outlet/>
+        </main>
+        <Footer/>
+      </div>
     </div>
-  )
+  ) : null
 }
 
 export default App
