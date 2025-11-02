28784c748cf9d7efa805af2deceab46e57c170bf
ez-sourav <souravb2003@gmail.com>
2025-11-02 16:51:01 +0530

    commit

--- src/components/Header/Button.jsx
+++ src/components/Header/Button.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Button = ({
-    children,
-    type = 'button',
-    bgColor = 'bg-blue-600',
-    textColor = 'text-white',
-    className = '',
-    ...props
-    }) => {
+  children,
+  type = "button",
+  bgColor = "bg-blue-600",
+  textColor = "text-white",
+  className = "",
+  ...props
+}) => {
   return (
-    <button 
-    className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
-    {...props}
+    <button
+      className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
+      {...props}
     >
-        {children}
+      {children}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
