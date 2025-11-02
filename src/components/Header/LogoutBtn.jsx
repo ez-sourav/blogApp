702bcafb851d88ca7624d293ce1c088173a8280c
ez-sourav <souravb2003@gmail.com>
2025-11-02 14:49:51 +0530

    commit

---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,21 @@
+import { useDispatch } from "react-redux"
+import { logout } from "../../store/authSlice"
+import authService from "../../appwrite/auth"
+
+const LogoutBtn = () => {
+  const dispatch = useDispatch()
+  const logouHandler = () => {
+    authService.logout().then( () => {
+      dispatch(logout())
+    })
+  }
+  return (
+    <button
+    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-ful"
+    >
+      Logout
+    </button>
+  )
+}
+
+export default LogoutBtn
