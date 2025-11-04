e68ca0641b1e95dd048c365ddcd6255ff06e3587
ez-sourav <souravb2003@gmail.com>
2025-11-04 22:23:51 +0530

    add PostForm

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import { useCallback } from "react"
+import { useForm } from "react-hook-form"
+import {Button,Input,Select,RTE} from '../index'
+import appwriteService  from '../../appwrite/config2'
+import { useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 const PostForm = () => {
   return (
