ba182d093e79f8dd7b328ad6c1b6cd1fda549ec5
ez-sourav <souravb2003@gmail.com>
2025-11-04 22:21:29 +0530

    add RTE

--- src/components/index.js
+++ src/components/index.js
@@ -6,9 +6,16 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
 import Select from "./Select";
-
-
+import RTE from "./RTE";
 
 export {
-    Header,Footer,Container,Logo,LogoutBtn,Input,Button,Select
-}
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Input,
+  Button,
+  Select,
+  RTE,
+};
